eff97bc63b62d994f485f18bfd71fd62990d3a6a
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-10-23 15:52:14 +0900

    Mock api with msw

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,6 +1,8 @@
 import InboxScreen from "./InboxScreen"
 import store from "../lib/store"
 import { Provider } from "react-redux"
+import { rest } from "msw";
+import { MockedState } from './TaskList.stories';
 
 export default {
   component: InboxScreen,
@@ -11,4 +13,29 @@ export default {
 const Template = () => <InboxScreen />
 
 export const Default = Template.bind({})
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      )
+    ]
+  }
+};
+
 export const Error = Template.bind({})
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
